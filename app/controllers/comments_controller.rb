588349c35b52cab28b14212133d355e1141a7781
Gabriel Ferreira <gabreeiel@gmail.com>
2015-11-07 15:37:53 +0000

    Suporte a comentarios completo, podendo destruir um comentario, enviar comentario pela interface, e com menssagens de alerta.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  def create
+    @job = Job.find(params[:job_id])
+    @comment = @job.comments.build(comment_params)
+    if @comment.save
+      flash[:notice] = "Comment was created with success!"
+    else
+      flash[:alert] = "Please fill in all fields to create a comment."
+    end
+    redirect_to @job
+  end
+  def destroy
+  	@comment = Comment.find(params[:id])
+  	@comment.destroy
+  	redirect_to @comment.job, notice: "Comment destroyed with success."
+  end
+  def comment_params
+  	params.require(:comment).permit(:name, :body)
+  end
+end
