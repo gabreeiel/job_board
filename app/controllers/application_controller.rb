a00a644772bd1e4f2ffaa28727f059fcd866e688
Gabriel Ferreira <gabreeiel@gmail.com>
2015-11-10 01:27:48 +0000

    v1.3.0 - Traduzido parcialmente em dois idiomas Ingles/portugues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_locale
   private
 
   def current_company
@@ -10,4 +11,12 @@ class ApplicationController < ActionController::Base
     end
   end
   helper_method :current_company
+
+  def set_locale
+    locale = params[:locale] || cookies[:locale]
+    if locale.present?
+      I18n.locale = locale
+      cookies[:locale] = { value: locale, expires: 30.days.from_now}
+    end
+  end
 end
